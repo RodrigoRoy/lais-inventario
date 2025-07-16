9f97b31d5f6ed0521d22f8b4b32baacf0c0c46a6
EderBerber <samuel_berber@ciencias.unam.mx>
2025-07-15 22:31:33 -0600

    Se cambió el menú NavBar por Steppers.

--- app.vue
+++ app.vue
@@ -1,5 +1,36 @@
 <template>
   <UApp>
+
+    <div class="w-full max-w-lg mx-auto my-6">
+      <UStepper :items="steps" v-model="activeStepIndex" disabled size="xs" />
+    </div>
+
+    <UContainer>
+      <NuxtPage />
+    </UContainer>
+
+  </UApp>
+</template>
+
+<script setup>
+
+const route = useRoute()
+
+// Define los pasos con títulos e íconos
+const steps = ref([
+  { title: 'Inicio', icon: 'i-mdi-home', to: '/' },
+  { title: 'Selección', icon: 'i-mdi-video-input-component', to: '/formulario' },
+  { title: 'Preliminar', icon: 'i-mdi-clipboard-check-outline', to: '/preliminar' }
+])
+
+const activeStepIndex = computed(() => {
+  const index = steps.value.findIndex(step => step.to === route.path)
+  return index
+})
+</script>
+
+<!-- <template>
+  <UApp>
     <UNavigationMenu :items="navBar" class="w-full justify-center" />
 
     <UContainer>
@@ -33,29 +64,4 @@ const navBar = ref([
   //   to: '/documento',
   // },
 ])
-
-
-/*const navBar = ref([
-  {
-    label: 'Inicio',
-    icon: 'i-mdi-home',
-    to: '/',
-    disabled: true
-  },
-  {
-    label: 'Crear salida',
-    icon: 'i-mdi-video-input-component',
-    to: '/formulario',
-  },
-  {
-    label: 'Preliminar',
-    icon: 'i-mdi-clipboard-check-outline',
-    to: '/preliminar',
-  },
-  // {
-  //   label: 'Documento',
-  //   icon: 'i-mdi-file-document',
-  //   to: '/documento',
-  // },
-])*/
-</script>
+</script> -->
