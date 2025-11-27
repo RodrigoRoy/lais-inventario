0485c194052007ef5ce0c9bb04494b8aec6fcaac
Rodrigo Colín <rodrigo.cln@ciencias.unam.mx>
2025-11-27 12:05:53 -0600

    fix: pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,9 +2,9 @@ module.exports = {
     apps: [
         {
             name: 'SalidasEquipoAudiovisual',
-            port: '3000',
-            // exec_mode: 'cluster',
-            // instances: 'max',
+            port: '3001',
+            exec_mode: 'cluster',
+            instances: 'max',
             script: './.output/server/index.mjs'
         }
     ]
